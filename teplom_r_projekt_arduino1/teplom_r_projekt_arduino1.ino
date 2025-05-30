013f1d889274684c0533cc2d56f028e1f2c1d583
Salinar1 <40271551+xtomasnemec@users.noreply.github.com>
2025-05-30 21:51:35 +0200

    again, uprava aby to fungovalo s tim senzorem a knihovnama, tedka je to na tobe rodricku

--- teplom_r_projekt_arduino1/teplom_r_projekt_arduino1.ino
+++ teplom_r_projekt_arduino1/teplom_r_projekt_arduino1.ino
@@ -1,21 +1,19 @@
 #include <Wire.h>
 #include "LiquidCrystal_I2C.h"
-#include "DHT11.h"
-
-LiquidCrystal_I2C lcd(0x20, 16, 2);
-
-// CALLBACKY PRO ARDUINO - uprav si to pro jiny mikrokontrolery
-void myPinMode(int pin, int mode) { pinMode(pin, mode); }
-void myDigitalWrite(int pin, int value) { digitalWrite(pin, value); }
-int myDigitalRead(int pin) { return digitalRead(pin); }
-void myDelay(unsigned long ms) { delay(ms); }
-
-DHT11 dht11(4, myPinMode, myDigitalWrite, myDigitalRead, myDelay);
+#include "DHT.h"
 
 const int Tlacitko1 = 8;
 const int Tlacitko2 = 7;
 int buttonState = HIGH;
 bool Normalnijednotky = true;
+
+// Vyber JEDEN řádek podle svého displeje:
+//LiquidCrystal_I2C lcd(0x27, 20, 4); // pro 20x4 displej
+LiquidCrystal_I2C lcd(0x20, 16, 2); // pro 16x2 displej 
+
+#define DHTPIN 4
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   pinMode(Tlacitko1, INPUT_PULLUP);
@@ -26,30 +24,36 @@ void setup() {
   lcd.print("Teplomer");
   delay(1000);
   lcd.clear();
+  dht.begin();
 }
 
 void loop() {
-  int teplota = dht11.readTemperature();
+  float teplota = dht.readTemperature();
+  float vlhkost = dht.readHumidity();
 
-  for (int i = 0; i < 2; i++) {
-    for (int j = 0; j < 1; j++) {
-      lcd.setCursor(0, 0);
-      lcd.print("Teplota:         ");
-      lcd.setCursor(9, 0);
-      if (Normalnijednotky) {
-        lcd.print(teplota);
-        lcd.print(" C ");
-      } else {
-        lcd.print(teplota * 9 / 5 + 32);
-        lcd.print(" F ");
-      }
-      delay(500);
+  if (isnan(teplota) || isnan(vlhkost)) {
+    lcd.print("Chyba senzoru");
+  } else {
+    lcd.setCursor(0, 0);
+    lcd.print("Teplota:");
+    lcd.setCursor(8, 0);
+    if (Normalnijednotky) {
+      lcd.print(teplota);
+      lcd.print("C");
+    } else {
+      lcd.print(teplota * 9 / 5 + 32);
+      lcd.print("F");
     }
+    lcd.setCursor(0, 1);
+    lcd.print("Vlhkost:");
+    lcd.setCursor(8, 1);
+    lcd.print(vlhkost);
+    lcd.print("%");
   }
 
   if (digitalRead(Tlacitko1) == LOW) {
     Normalnijednotky = !Normalnijednotky;
     lcd.clear();
-    delay(300);
+    delay(500);
   }
 }

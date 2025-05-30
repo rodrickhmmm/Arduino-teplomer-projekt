ef3a438921a9e6dc41cb421b9a1c6c08089fd6ce
Salinar1 <40271551+xtomasnemec@users.noreply.github.com>
2025-05-30 22:09:52 +0200

    par komentu, uz ses na to sam rodricku

--- teplomer projekt/teplom_r_projekt_arduino1.ino
+++ teplomer projekt/teplom_r_projekt_arduino1.ino
@@ -2,58 +2,58 @@
 #include "LiquidCrystal_I2C.h"
 #include "DHT.h"
 
-const int Tlacitko1 = 8;
-const int Tlacitko2 = 7;
+//Nastaveni
+const int Tlacitko1 = 8; // pin praveho tlacitka
+const int Tlacitko2 = 7; //pin levehoi tlacitka
+#define DHTPIN 4 // pin senzoru
+#define DHTTYPE DHT11 // typ senzoru
+// Vyber JEDEN řádek podle svého displeje:
+
+//LiquidCrystal_I2C lcd(0x27, 20, 4); // pro 20x4 displej
+//LiquidCrystal_I2C lcd(0x20, 16, 2); // pro 16x2 displej 
+
+// inicializace promenych
+//tohohle si nevsimej, je to jenom aby ide nevyhazovalo chyby
 int buttonState = HIGH;
 bool Normalnijednotky = true;
 
-// Vyber JEDEN řádek podle svého displeje:
-//LiquidCrystal_I2C lcd(0x27, 20, 4); // pro 20x4 displej
-LiquidCrystal_I2C lcd(0x20, 16, 2); // pro 16x2 displej 
-
-#define DHTPIN 4
-#define DHTTYPE DHT11
+// ZACATEK KODU, NEHRAB SE TU!
+// init senzoru
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  pinMode(Tlacitko1, INPUT_PULLUP);
+  pinMode(Tlacitko1, INPUT_PULLUP); // nastaveni tlacitek
   pinMode(Tlacitko2, INPUT_PULLUP);
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print("Teplomer");
+  lcd.print("Teplomer"); // splash screen
   delay(1000);
   lcd.clear();
-  dht.begin();
+  dht.begin(); //aktivace senzoru
 }
 
 void loop() {
-  float teplota = dht.readTemperature();
-  float vlhkost = dht.readHumidity();
-
-  if (isnan(teplota) || isnan(vlhkost)) {
-    lcd.print("Chyba senzoru");
+  float teplota = dht.readTemperature(); //cteni teploty
+  if (isnan(teplota)) {
+    lcd.print("Chyba senzoru"); // chyba kdyz se posere HARDWARE senzoru
   } else {
+    //rederovani teploty
     lcd.setCursor(0, 0);
     lcd.print("Teplota:");
     lcd.setCursor(8, 0);
-    if (Normalnijednotky) {
+    if (Normalnijednotky) { // normalni jednotky - stupne Celsia, nenormalni jednotky pro ameicany - frenheit
       lcd.print(teplota);
       lcd.print("C");
     } else {
-      lcd.print(teplota * 9 / 5 + 32);
+      lcd.print(teplota * 9 / 5 + 32); //nejakej vzorecek z googlu na prevod jednotek
       lcd.print("F");
-    }
-    lcd.setCursor(0, 1);
-    lcd.print("Vlhkost:");
-    lcd.setCursor(8, 1);
-    lcd.print(vlhkost);
-    lcd.print("%");
-  }
-
-  if (digitalRead(Tlacitko1) == LOW) {
-    Normalnijednotky = !Normalnijednotky;
+      }
+    // konec renderovani teploty
+  if (digitalRead(Tlacitko1) == LOW) { //ty tlacitka jsou zapojeny jako pull-up, takze LOW znamena stisknuti
+    Normalnijednotky = !Normalnijednotky; //negace hodnoty normalni jednotky - prepne mezi mezi C a F
     lcd.clear();
-    delay(500);
+    delay(500); // delay aby se to tlacitko neregistrovalo vickrat
+    }
   }
 }

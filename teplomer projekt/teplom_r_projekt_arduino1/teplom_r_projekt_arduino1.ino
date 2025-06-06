fc39dfc08d12ec92020f75137b774ac32fc2da16
Rodrick__ <tom.rodrick100@gmail.com>
2025-06-06 21:25:09 +0200

    Tak, řekl bych, že toto už je skoro celé hotové, ještě dodělat komentáře a hotovo!!!

--- teplomer projekt/teplom_r_projekt_arduino1/teplom_r_projekt_arduino1.ino
+++ teplomer projekt/teplom_r_projekt_arduino1/teplom_r_projekt_arduino1.ino
@@ -13,11 +13,11 @@ const int Tlacitko2 = 7; //pin leveho tlacitka
 LiquidCrystal_I2C lcd(0x27, 16, 2); // pro 16x2 displej, pokud displej nefunguje tak můžete předělat adresu na 0x20 nebo 0x3F
 
 // inicializace promenych
-//tohohle si nevsimej, je to jenom aby ide nevyhazovalo chyby
+// Tohle je zde jenom aby IDE nevyhazovalo chyby
 int buttonState = HIGH;
 bool Normalnijednotky = true;
 
-// ZACATEK KODU, NEHRAB SE TU!
+// ZACATEK KODU, NEHRABAT SE TU!
 // init senzoru
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -59,7 +59,7 @@ byte sipkaChar[8]{
 
 void setup() {
   pinMode(Tlacitko1, INPUT_PULLUP); // nastaveni tlacitek
-  pinMode(Tlacitko2, INPUT_PULLUP);
+  pinMode(Tlacitko2, INPUT_PULLUP); // nastaveni tlacitek
   lcd.init(); //nastaveni displeje
   lcd.backlight();
   lcd.setCursor(0, 0); // nastavi aby se text/symboly/proste cokoliv zaclo psat na souradnici 0x0
@@ -75,48 +75,72 @@ void setup() {
   dht.begin(); //aktivace senzoru
 }
 
-
+float posledniTeplota = 0; // Globální proměnná pro poslední teplotu
+bool teplotaNactena = false; // Zda už byla teplota někdy načtena
+bool potrebaPrekreslit = true; // Překreslit displej?
 
 void loop() {
   float teplota = dht.readTemperature(); // inicializuje promennou na cteni teploty ze senzoru
 
   if (isnan(teplota)) {
+    // VŽDY zobraz error, když je chyba senzoru
+    lcd.clear();
     lcd.print("Error 621/gurt");//maly easter egg pro ty co to zapojí špatně xdd
     lcd.setCursor(0,1);
     lcd.print("Chyba senzoru!"); // chyba kdyz neni senzor nalezen nebo kdyz je jeho hardware poškozen, stručně řečeno pokud arduino nedostane danou teplotu od senzoru, tak to napíše tuto chybu
-    delay(300);
-    lcd.clear();
-  } else {
-    //Vypíše teplotu + znak stupně na displej
-    lcd.setCursor(0, 0); // nastavi aby se text/symboly/proste cokoliv zaclo psat na souradnici 0x0
-    lcd.print("Teplota:");  // napíše slovo Teplota
-    lcd.setCursor(5, 1); // nastavi aby se text/symboly/proste cokoliv zaclo psat na souradnici 5x1
-    lcd.write(byte(0)); // vypíše znak stupně. Pozor! Nelze použít lcd.print, jelikož daný symbol není definován jako ASCII symbol (písmeno, čísla, ...)
-    
-    // postranni menu (to kde se zobrazi C a F iyky)
-    lcd.setCursor(13, 0); // nastavi aby se text/symboly/proste cokoliv zaclo psat na souradnici 13x0
-    lcd.write(byte(1)); // vypíše znak svislé čáry. Pozor! Nelze použít lcd.print, jelikož daný symbol není definován jako ASCII symbol (písmeno, čísla, ...)
-    lcd.print("C"); // vypíše vedle svislé čáry znak C
-    lcd.setCursor(13, 1); // nastavi aby se text/symboly/proste cokoliv zaclo psat na souradnici 13x1
-    lcd.write(byte(1)); // vypíše znak svislé čáry. Pozor! Nelze použít lcd.print, jelikož daný symbol není definován jako ASCII symbol (písmeno, čísla, ...)
-    lcd.print("F"); // vypíše vedle svislé čáry znak F
-    lcd.setCursor(0, 1); // nastavi aby se dalsi text (v tomto pripade jiz teplota) psala na souradnici 0x1
-
-    if (Normalnijednotky) { // normalni jednotky - stupne Celsia, nenormalni jednotky pro ameicany - frenheit
-      lcd.print(teplota); // vypíše teplotu
-      lcd.setCursor(15, 0); // nastavi aby se dalsi text (v tomto pripade jiz teplota) psala na souradnici 15x0
-    lcd.write(byte(2)); // vypíše znak šipky. Pozor! Nelze použít lcd.print, jelikož daný symbol není definován jako ASCII symbol (písmeno, čísla, ...)
-    } else {
-      lcd.print(teplota * 9 / 5 + 32); // vzoreček na vypocet stupnu fahrenheitu ze stupnu celsia
-      lcd.setCursor(15, 1); // nastavi aby se dalsi text (v tomto pripade jiz teplota) psala na souradnici 15x1
-    lcd.write(byte(2)); // vypíše znak svislé čáry. Pozor! Nelze použít lcd.print, jelikož daný symbol není definován jako ASCII symbol (písmeno, čísla, ...)
-      }
-
-    // konec renderovani teploty
-  if (digitalRead(Tlacitko1) == LOW) { //ty tlacitka jsou zapojeny jako pull-up, takze LOW znamena stisknuti
-    Normalnijednotky = !Normalnijednotky; //negace hodnoty normalni jednotky - prepne mezi mezi C a F
-    lcd.clear();
-    delay(500); // delay aby se to tlacitko neregistrovalo vickrat
-    }
+    teplotaNactena = false;
+    delay(1000);
+    return;
   }
+  else{
+    // Pokud je stisknuto Tlacitko2, ulož novou teplotu
+  if (digitalRead(Tlacitko2) == LOW) {
+    posledniTeplota = teplota;
+    teplotaNactena = true;
+    potrebaPrekreslit = true;
+    delay(500); // Debounce tlačítka
+  }
+
+  // Přepínání jednotek zůstává na Tlacitko1
+  if (digitalRead(Tlacitko1) == LOW) {
+    Normalnijednotky = !Normalnijednotky;
+    potrebaPrekreslit = true;
+    delay(500);
+  }
+
+  // Překresli displej jen pokud je potřeba
+  if (potrebaPrekreslit) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Teplota:");
+    lcd.setCursor(5, 1);
+    lcd.write(byte(0));
+    
+    lcd.setCursor(13, 0);
+    lcd.write(byte(1));
+    lcd.print("C");
+    lcd.setCursor(13, 1);
+    lcd.write(byte(1));
+    lcd.print("F");
+    lcd.setCursor(0, 1);
+
+    if (teplotaNactena) {
+      if (Normalnijednotky) {
+        lcd.print(posledniTeplota);
+        lcd.setCursor(15, 0);
+        lcd.write(byte(2));
+      } else {
+        lcd.print(posledniTeplota * 9 / 5 + 32);
+        lcd.setCursor(15, 1);
+        lcd.write(byte(2));
+      }
+    } else {
+      lcd.print("----");
+    }
+    potrebaPrekreslit = false;
+  }
+
+  }
+
+  delay(50); // Krátká prodleva pro lepší odezvu
 }
